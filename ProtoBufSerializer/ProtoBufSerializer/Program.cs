43b20efe10540f9f6c7520cd8dd6f66cef832e13
minsung.bae <minsungbae@GKR-PCNM0007.local>
2016-01-05 11:27:40 +0900

    * add protobuf protocol sample * add protobuf serializer sample

---
+++ ProtoBufSerializer/ProtoBufSerializer/Program.cs
@@ -0,0 +1,20 @@
+﻿using ProtoBuf.Meta;
+using ProtobufBundle.Protocols.PBClass.Common;
+
+namespace ProtoBufSerializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var model = TypeModel.Create();
+            
+            model.Add(typeof(InfoScene), true);
+            
+            model.AllowParseableTypes = true;
+            model.AutoAddMissingTypes = true;
+
+            model.Compile("ProtoBufSerializer", "ProtoBufSerializer.dll");
+        }
+    }
+}
